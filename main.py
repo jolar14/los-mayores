d0ad036f31e9efab84336949fc155b8c1d55b85f
jolar14 <147890035+jolar14@users.noreply.github.com>
2023-10-13 22:33:51 -0300

    Create main.py
    aprende las palabras actuales

---
+++ main.py
@@ -0,0 +1,10 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+     print(meme_dict[word])
+else:
+     print("esta plabra no esta aca en el diccionario")
